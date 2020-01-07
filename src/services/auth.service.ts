6c6b69209904bf8c86752cb2c345cd50ba849b38
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-01-08 00:56:52 +0530

    [update] handle the session manually

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,12 +1,25 @@
+import chalk from "chalk";
 import { NextFunction, Request, Response } from "express";
 import passport from "passport";
 
 class AuthService {
 
+  /**
+   * Callback method for User login.
+   *
+   * Since we are using the custom callback for Authentication, hence
+   * manuallly handling the session by calling the `req.login` method.
+   *
+   * Doing this will make sure to send the session cookie in the response
+   * header and takes care of Serializing and De-serialzing the user.
+   */
   public loginUser(req: Request, res: Response, next: NextFunction): void {
-    passport.authenticate("local", (err, user, info: { status: number; }) => {
+    passport.authenticate("local", { session: true }, (err, user, info: { status: number; }) => {
       switch (info.status) {
         case 200:
+          req.login(user, () => {
+            console.log(chalk.green(`A session is created manually.`));
+          });
           res.status(200).json({
             message: `${user.firstName} ${user.lastName} logged in successfully!`
           });
@@ -33,6 +46,14 @@ class AuthService {
     })(req, res, next);
   }
 
+  /**
+   * Callback method for user logout
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @param {NextFunction} next
+   * @memberof AuthService
+   */
   public logoutUser(req: Request, res: Response, next: NextFunction): void {
     req.logout();
     res.status(200).json({
