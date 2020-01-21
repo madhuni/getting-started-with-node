80451fbee1a027fea1c39a4945f353b4a936b376
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-01-21 11:45:20 +0530

    [refactor] Migrate from MongoDB to MySQL

--- src/config/passport.config.ts
+++ src/config/passport.config.ts
@@ -1,33 +1,35 @@
 import bcrypt from "bcrypt";
+import { MysqlError } from "mysql";
 import { PassportStatic } from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 
-import { User } from "../models/user.model.old";
+import { connection as MySQL } from "../config/mysql.config";
+import { User } from "../models/user.model";
 
 export function setupPassportStrategy(passport: PassportStatic) {
   passport.use(new LocalStrategy((username: string, password: string, done: CallableFunction) => {
-    const query = {userName: username};
-    User.findOne(query)
-      .then((user: any) => {
-        if (!user) {
-          return done(null, false, { status : 404 });
-        }
-
-        // If user is found, then match password
-        bcrypt.compare(password, user.password, (err: Error, isMatch: boolean) => {
-          if (err) {
-            return done(err, false, { status: 500 });
-          }
-          if (isMatch) {
-            return done(null, user, { status: 200 });
-          } else {
-            return done(null, user, { status: 400 });
-          }
-        });
-      })
-      .catch((err: Error) => {
+    const sqlQuery = `SELECT * FROM users WHERE username = ${MySQL.escape(username)}`;
+    MySQL.query(sqlQuery, (err: MysqlError, results: User[]) => {
+      if (err) {
         console.log(err);
+        return;
+      }
+      if (!results.length) {
+        return done(null, false, { status : 404 });
+      }
+      const user: User = results[0];
+      // If user is found, then match password
+      bcrypt.compare(password, user.password, (err: Error, isMatch: boolean) => {
+        if (err) {
+          return done(err, false, { status: 500 });
+        }
+        if (isMatch) {
+          return done(null, user, { status: 200 });
+        } else {
+          return done(null, user, { status: 400 });
+        }
       });
+    });
   }));
 
   passport.serializeUser((user: any, done: CallableFunction) => {
@@ -35,8 +37,9 @@ export function setupPassportStrategy(passport: PassportStatic) {
   });
 
   passport.deserializeUser((id: string, done: CallableFunction) => {
-    User.findById(id, (err: Error, user: any) => {
-      done(err, user);
+    const sqlQuery = `SELECT * FROM users WHERE id = ${MySQL.escape(id)}`;
+    MySQL.query(sqlQuery, (err: MysqlError, results: User[]) => {
+      done(err, results[0]);
     });
   });
 }
---
+++ src/models/user.model.ts
@@ -0,0 +1,13 @@
+/**
+ * Interface for the User
+ * @export
+ * @interface User
+ */
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  userName: string;
+  password: string;
+}
