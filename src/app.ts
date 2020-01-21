a7e8ebead21cc65722cf906c1d289adc52af0774
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-01-21 11:49:37 +0530

    remove comments regarding MongoDB setup

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,11 @@ import chalk from "chalk";
 import cors from "cors";
 import express, { Application } from "express";
 import session from "express-session";
-// import mongoose, { Mongoose } from "mongoose";
 import { MysqlError } from "mysql";
 import passport from "passport";
 import * as path from "path";
 
 import { options as corsConfig } from "./config/cors.config";
-// import { dbOptions, localConnectionString } from "./config/db.config";
 import { connection } from "./config/mysql.config";
 import { setupPassportStrategy } from "./config/passport.config";
 import { options as sessionConfig } from "./config/session.config";
@@ -27,7 +25,6 @@ class App {
     this.app = express();
     setupPassportStrategy(passport); // setup passport strategy before setting up the middlewares
     this.setMiddlewares();
-    // this.setMongoConfig();
     this.setMysqlConnection();
     this.setControllers();
   }
@@ -42,26 +39,6 @@ class App {
     this.app.use("/", logIncomingRequestDetails);
     this.app.use("/assets", express.static(path.join(__dirname + "/public")));
   }
-
-  /** Keeping the code for future reference */
-  // private setMongoConfig(): void {
-  //   mongoose.connect(localConnectionString, dbOptions)
-  //     .then((res: Mongoose) => {
-  //       console.log(
-  //         chalk.green(`Connected to MongoDB successfully!`)
-  //       );
-  //     })
-  //     .catch((err: Error) => {
-  //       console.log(chalk.red(`Unable to Connect to the MongoDB! Terminating the process.`), err);
-  //       process.exit(1);
-  //     });
-
-  //   mongoose.connection.on("disconnected", () => {
-  //     console.error(
-  //       chalk.red("MongoDB disconnected!")
-  //     );
-  //   });
-  // }
 
   private setMysqlConnection(): void {
     connection.connect((err: MysqlError) => {
